033cd6ae9d2747af20ac56773163180d1a4aa942
NrmeenAraby <115955249+NrmeenAraby@users.noreply.github.com>
2025-09-14 18:54:24 +0300

    fruits have been created and explode them

---
+++ Assets/Fruits.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruits : MonoBehaviour {
+
+	public GameObject slicedFruitPrefab;
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) { 
+		   createSlicedFruit();
+		}
+	}
+
+	public void createSlicedFruit()
+	{
+		GameObject created = Instantiate(slicedFruitPrefab, transform.position, transform.rotation);
+		Rigidbody[] rbOnSliced = created.GetComponentsInChildren<Rigidbody>();
+		foreach(Rigidbody rb in rbOnSliced)
+		{
+			rb.transform.rotation = Random.rotation;
+			rb.AddExplosionForce(Random.Range(500, 1000), transform.position, 5f);
+			
+		}
+		Destroy(gameObject);
+	}
+}
