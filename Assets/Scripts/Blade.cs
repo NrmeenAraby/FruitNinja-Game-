843897e4c1e22d655a4ee4a5bc93d59cfb9e563c
NrmeenAraby <115955249+NrmeenAraby@users.noreply.github.com>
2025-09-16 22:56:06 +0300

    only activate the collider of the blade when the mouse really moves, so no sliced fruitd if it hits the cursor randomly without moving

--- Assets/Scripts/Blade.cs
+++ Assets/Scripts/Blade.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class Blade : MonoBehaviour {
 
 	private Rigidbody2D rigidbody;
+
+
+	public float minDist = 0.1f;
+	private Vector3 lastMousePosition;
+	private Collider2D collider;
 	void Awake()
 	{
 		rigidbody = GetComponent<Rigidbody2D>();	
+		collider= GetComponent<Collider2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+	    collider.enabled = IsMouseMoving();
 		SetBladeToMouse();
 	}
 	private void SetBladeToMouse()
@@ -19,5 +26,12 @@ public class Blade : MonoBehaviour {
 		var mousePosition = Input.mousePosition;
 		mousePosition.z = 10; //solves some 3d and 2d problem!
 		rigidbody.position = Camera.main.ScreenToWorldPoint(mousePosition);
+	}
+	private bool IsMouseMoving()
+	{
+		Vector3 currMousePosition = transform.position;
+		float tarveled = (lastMousePosition - currMousePosition).magnitude;
+		lastMousePosition = currMousePosition;
+		return (tarveled>minDist)? true:false;
 	}
 }
