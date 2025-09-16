dff0dd08404ef6fefd338bf1313fedace2a7d328
NrmeenAraby <115955249+NrmeenAraby@users.noreply.github.com>
2025-09-16 21:17:26 +0300

    Best Score ahs been added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,32 +6,46 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour {
     [Header("Score Elements")]
     public int score;
+    public int highScore;
     public Text scoreText;
+    public Text highScoreText;
 
     [Header("GameOver")]
     public GameObject gameOverPanel;
     public Text gameOverScorePanelText;
-
+    public Text gameOverBestScorePanelText;
     public void Awake()
     {
         gameOverPanel.SetActive(false);
+        GetScore();
+    }
+    private void GetScore()
+    {
+        highScore = PlayerPrefs.GetInt("Highscore");
+        highScoreText.text = "Best: " + highScore.ToString();
     }
     public void IncreaseScore(int points)
     {
         score += points;
         scoreText.text=score.ToString();
+        if (score > highScore) {
+            PlayerPrefs.SetInt("Highscore", score);
+            highScore = score;
+            highScoreText.text="Best: "+highScore.ToString();
+        }
     }
     public void OnBombHit() {
         Time.timeScale = 0;  //Stop the game
         gameOverScorePanelText.text="Score: "+score.ToString();
+        gameOverBestScorePanelText.text = "Best: " + highScore.ToString();
         gameOverPanel.SetActive(true);
     }
     public void RestartGame()
     {
         score = 0;
-        scoreText.text = "0";
+        scoreText.text = score.ToString();
         gameOverPanel.SetActive(false);
-        gameOverScorePanelText.text = "Score: 0";
+
         foreach(GameObject g in GameObject.FindGameObjectsWithTag("Interactable"))
         {
             Destroy(g);
